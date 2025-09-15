c02c4c3553b1e8cf57b628eaedbd7b0c57d9bb74
Adam Price <adam@immunera.ai>
2025-09-15 18:16:29 -0400

    Use importlib.resources instead of pkg_resources

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin env python
 from distutils.core import setup
+import sys
+
+install_requires=['tqdm']
+
+if sys.version_info < (3, 9):
+    install_requires.append('importlib_resources>=1.3.0')
+
 
 setup(
     name='crowelab_pyir',
-    version='1.6.1',
+    version='1.6.2',
     description='',
     license='Creative Commons Attribution 4.0',
     author='Sam Day, Andre Branchizio, Jordan Willis, Jessica Finn, Taylor Jones, Sam Schmitz, Luke Myers',
     author_email='samuel.day@vumc.org, andrejbranch@gmail.com, jwillis0720@gmail.com, strnad.bird@gmail.com',
     scripts=['./bin/pyir'],
-    install_requires=['tqdm'],
+    install_requires=install_requires,
     packages=['crowelab_pyir'],
     package_dir={'crowelab_pyir': './pyir'},
     package_data={'crowelab_pyir': ['data/*',
@@ -37,4 +44,5 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
+    python_requires='>=3.6',
 )
